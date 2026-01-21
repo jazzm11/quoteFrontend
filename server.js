47027a3321325e222993514d33a788615bcead8b
Jazz Maquiraya <jazzm11>
2026-01-21 11:34:31 +0100

    Oppdatert cors origin 1.1

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:4000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 // Session middleware
